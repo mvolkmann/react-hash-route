e3d2d746d6107d11606c16776ed406b09cdd6da4
Jason Schindler <jason@types.codes>
2018-04-18 22:28:26 -0500

    Adds subscription mechanism for hash changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+let hashSubs;
+
 function getHash() {
   const {hash} = window.location;
   const parts = hash.split('/'); // separates hash parameters
@@ -15,6 +17,42 @@ function getHashParameters() {
   return parts;
 }
 
+function setupListener(render) {
+  hashSubs = [];
+
+  window.addEventListener('hashchange', () => {
+    render();
+    const hash = getHash();
+    const parameters = getHashParameters();
+    hashSubs.forEach(sub => {
+      setTimeout(sub, 0, hash, parameters);
+    });
+  });
+}
+
+/**
+ * Can add a handler to be called when the
+ * hash changes.
+ *
+ * On a change event, the handler will be
+ * called with an object that contains the
+ * current `hash` and `parameters`.
+ *
+ * Returns a function that can be called
+ * to unsubscribe the handler.  If this
+ * is being used in a component, the
+ * unsubscribe should be called when the
+ * component is unmounted to prevent
+ * memory leaks.
+ *
+ */
+function onHashChange(handler) {
+  hashSubs = hashSubs.concat(handler);
+  return () => {
+    hashSubs = hashSubs.filter(sub => sub !== handler);
+  };
+}
+
 /**
  * Changes the current "route"
  * and returns null so render methods can
@@ -27,8 +65,8 @@ function route(hash) {
 }
 
 function routeSetup(render) {
-  window.addEventListener('hashchange', render);
+  setupListener(render);
   render();
 }
 
-module.exports = {getHash, getHashParameters, route, routeSetup};
+module.exports = {getHash, getHashParameters, onHashChange, route, routeSetup};
